65a783aeddf76b7aba6ef25aa9f7c3338f17e647
Rufat-J <rj.7@live.se>
2023-02-16 17:18:10 +0100

    fixed the skeleton and added API

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,62 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [data, setData] = useState({})
+    const [location, setLocation] = useState('')
 
-  return (
-    <div className="App">
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f5a99af417b3acca22a2ecd906a58d2e`
 
-    </div>
-  )
+    const searchLocation = (event) => {
+        if (event.key === 'Enter') {
+            axios.get(url).then((response) => {
+                setData(response.data)
+                console.log(response.data)
+            })
+            setLocation('')
+        }
+    }
+
+    return (
+        <div className="app">
+            <div className="search">
+                <input
+                    value={location}
+                    onChange={event => setLocation(event.target.value)}
+                    onKeyDown={searchLocation}
+                    placeholder='Enter Location'
+                    type="text"/>
+            </div>
+            <div className="container">
+                <div className="top">
+                    <div className="location">
+                        <p>{data.name}</p>
+                    </div>
+                    <div className="temp">
+                        {data.main ? <h1>{data.main.temp}°F</h1> : null}
+                    </div>
+                    <div className="description">
+                        {data.weather ? <p>{data.weather[0].main}</p> : null }
+                     </div>
+                </div>
+                <div className="bottom">
+                    <div className="feels">
+                        {data.main ? <p className="bold">{data.main.feels_like}°F</p> : null}
+                        <p>Feels Like</p>
+                    </div>
+                    <div className="humidity">
+                        {data.main ? <p className="bold">{data.main.humidity}%</p> : null }
+                        <p>{data.main[1]}</p>
+                    </div>
+                    <div className="wind">
+                        <p className="bold">12 MPH</p>
+                        <p>Winds</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default App
